a771df2c8ac87ff224098efd62161d40e626382d
atulsingla82 <asingla@wgu.edu>
2017-09-04 21:04:36 -0400

    Added search query

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
 import React,{Component} from 'react';
-import {Button, Form, FormControl} from 'react-bootstrap'
+import { FormGroup,InputGroup, Glyphicon, FormControl} from 'react-bootstrap'
+import './App.css'
+
 export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+    
+    query:" "
+    	
+    }
+
+  }
+   
+   search() {
+    
+    console.log('this.state',this.state)
+
+   }
 
   render (){
   return(
      
-      <div> 
+      <div className="App"> 
        <div className="App-title"> Music Master </div>
-      <div>
-      <input placeholder =" search for an artist.." />
-      <Button > Submit </Button>
+      
+      <FormGroup>
+        <InputGroup>
+          <FormControl
+          value={this.state.query}
+          onChange = {event =>{this.setState({query:event.target.value})}}
+          onKeyPress = {event => {
+            
+            if(event.key === 'Enter'){
+            
+            this.search()
 
-       </div>
+            }
+
+          }}
+         type="text"
+         placeholder ="search for an artist.."/>
+         <InputGroup.Addon onClick = {()=>this.search()}>
+         <Glyphicon glyph="search"></Glyphicon>
+         </InputGroup.Addon>
+       </InputGroup>
+     </FormGroup>
+      
         <div className="Profile"> 
          <div> Artist Profile </div>
          <div> Artist Picture </div>
